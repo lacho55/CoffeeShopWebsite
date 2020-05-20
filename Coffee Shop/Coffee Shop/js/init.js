a79928fb1c83094435ddcec543744d1ad317ba38
lacho55 <lacho_fitness@abv.bg>
2020-05-20 23:41:14 +0300

    School project

---
+++ Coffee Shop/Coffee Shop/js/init.js
@@ -0,0 +1,19 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+//JQuerry for slider
+$(document).ready(function(){
+    $('.slider').slider();
+  });
+
+
+//This is for a special picture view
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
